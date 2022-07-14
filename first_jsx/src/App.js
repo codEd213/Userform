2a2676d750a7dd3729d45abe1166a060d4fc9004
Edwin Soto <edwin_s1106@yahoo.com>
2022-07-13 18:33:51 -0700

    completed first_jsx assignment

---
+++ first_jsx/src/App.js
@@ -0,0 +1,18 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <h1 className="App">Hello Dojo</h1>
+      <h2>Things I Need To Do: </h2>
+      <ul className="list">
+        <li>Learn React</li>
+        <li>Learn how to make this look cleaner</li>
+        <li>Learn</li>
+      </ul>
+    </div>
+  );
+}
+
+export default App;
