e8f0831d8bae2e75a0df73d465dc07b1cfec64e5
Edwin Soto <edwin_s1106@yahoo.com>
2022-07-17 20:54:27 -0700

    completed 'putting it together' assignment

--- prop_it/src/App.js
+++ prop_it/src/App.js
@@ -2,11 +2,12 @@ import logo from "./logo.svg";
 import "./App.css";
 import PersonCard from "./components/PersonCard";
 import PersonCard2 from "./components/PersonCard2";
+import React, { useState } from "react";
 
 function App() {
   const person2 = {
     firstName: "Ed",
-    lastName: "So",
+    lastName: "Soto",
     age: 25,
     hairColor: "black",
   };
@@ -22,6 +23,7 @@ function App() {
     age: 32,
     hairColor: "red",
   };
+
   return (
     <div className="App">
       <PersonCard2 firstName="Jane" lastName="Doe" age="28" hairColor="Black" />
--- prop_it/src/components/PersonCard.js
+++ prop_it/src/components/PersonCard.js
@@ -1,14 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 
 const PersonCard = (props) => {
   const { person } = props;
+  const [age, setAge] = useState(person.age);
+  const handleAge = () => {
+    setAge(age + 1);
+  };
   return (
     <div className="App">
       <h1>
         {person.lastName}, {person.firstName}
       </h1>
-      <h2>Age: {person.age}</h2>
+      <h2>Age: {age}</h2>
       <h2>Hair Color: {person.hairColor}</h2>
+      <button onClick={handleAge}>
+        Birthday Button for {person.firstName}
+      </button>
     </div>
   );
 };
